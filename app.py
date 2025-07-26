c0f4f990fb1d7f3eec05faf366109ce3713ca27b
yamasaki-1205 <FCA22060024.edu.fca.ac.jp>
2025-07-27 03:29:13 +0900

    ねこ

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from logic import get_cat_images_by_breed
 
 st.title("猫の種類で画像検索アプリ")
 
-breed_input = st.text_input("猫の種類名を入力してください（例: bengal, siamese）")
+breed_input = st.text_input("猫の種類名を入力してください（例: サイベリアン, マンチカン）")
 
 if breed_input:
     with st.spinner("画像を取得中…"):
